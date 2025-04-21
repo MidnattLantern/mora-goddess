64ec897e35ad16f19b2a90a91e3404e8171ec8b5
Alma <isakvent@outlook.com>
2025-04-21 19:27:52 +0200

    create view for view from outside project

---
+++ mora-goddess/src/components/projectViewFromOutside/ProjectViewFromOutside.tsx
@@ -0,0 +1,39 @@
+import Styles from "./ProjectViewFromOutside.module.css";
+import SmallViewFromOutside from "../../assets/downloadable/mora-temple-v2-1mb.jpg"
+import LargeViewFromOutside from "../../assets/downloadable/mora-temple-v2-6mb.png";
+import DownloadIcon from "../../assets/svg/DownloadIcon.svg?react";
+import TurnPageIcon from "../../assets/svg/TurnPageIcon.svg?react";
+
+const ProjectViewFromOutside = () => {
+    return (
+        <>
+            <div className={Styles.Foundation}>
+                <div className={Styles.TitleContainer}>
+                    <h1 className={Styles.TitleText}>{"View from outside"}</h1>
+                </div>
+
+                <div className={Styles.ImageAndNavigationContainer}>
+                    <div className={Styles.ImageContainer}>
+                        <img className={Styles.ImageSize} src={SmallViewFromOutside} alt={"Failed to load image: View from outside"}/>
+                    </div>
+                    <div className={Styles.NavigationBarContainer}>
+                        <a className={Styles.DownloadButton} href={SmallViewFromOutside} download={"view from outside"}>
+                            <DownloadIcon className={Styles.DownloadIcon}/>
+                            {"As jpg 1mb"}
+                        </a>
+                        <a className={Styles.DownloadButton} href={LargeViewFromOutside} download={"view from outside"}>
+                            <DownloadIcon className={Styles.DownloadIcon}/>
+                            {"As png 6mb"}
+                        </a>
+                        <a className={Styles.LinkToWeblogButton} href={"./view-from-outside"}>
+                            {"Read the Weblog"}
+                            <TurnPageIcon className={Styles.TurnPageIcon}/>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+}
+export default ProjectViewFromOutside;
--- mora-goddess/src/pages/mainView/MainView.tsx
+++ mora-goddess/src/pages/mainView/MainView.tsx
@@ -1,27 +1,16 @@
 import Styles from "./MainView.module.css";
-import SmallViewFromOutside from "../../assets/downloadable/mora-temple-v2-1mb.jpg"
-import LargeViewFromOutside from "../../assets/downloadable/mora-temple-v2-6mb.png";
 import ProjectViewFromInside from "../../components/projectViewFromInside/ProjectViewFromInside";
+import ProjectViewFromOutside from "../../components/projectViewFromOutside/ProjectViewFromOutside";
 
 const MainView = () => {
 
     return(<>
         <div className={Styles.Foundation}>
-
-        <ProjectViewFromInside />
-
-        <h1>{"View from outside"}</h1>
-          <img className={Styles.ImageAppearance} src={SmallViewFromOutside} alt={"Failed to load image: View from inside"}/>
-
-        <div className={Styles.DownloadButtonsView}>
-            <a className={Styles.DownloadButton} href={SmallViewFromOutside} download="Mora Goddess V2">
-            {"Download as jpg 1mb"}
-            </a>
-            <a className={Styles.DownloadButton} href={LargeViewFromOutside} download="Mora Goddess V2">
-            {"Download as png 6mb"}
-            </a>
-        </div>
-        
+            <br/>
+            <ProjectViewFromInside />
+            <br/>
+            <ProjectViewFromOutside />
+            <br/>
         </div>
     </>)
 };
