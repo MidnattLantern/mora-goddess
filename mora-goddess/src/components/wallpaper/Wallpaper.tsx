19592d78aedb94a7dd18a44c6b7d661421cedf81
Alma <isakvent@outlook.com>
2025-04-24 02:55:20 +0200

    css responsive cleanup

--- mora-goddess/src/components/wallpaper/Wallpaper.tsx
+++ mora-goddess/src/components/wallpaper/Wallpaper.tsx
@@ -1,7 +1,10 @@
 import Styles from './Wallpaper.module.css';
 import WallpaperAsset from '../../assets/downloadable/mora-temple-v1-1mb.jpg';
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 const Wallpaper = () => {
+    const { width } = useWindowSize();
+
     const Overlay = () => {
         return (
             <div className={Styles.Overlay} />
@@ -10,7 +13,11 @@ const Wallpaper = () => {
 
     return (
         <div className={Styles.Foundation}>
-            <img className={Styles.ImageAppearance} src={WallpaperAsset} alt={"Failed to load image: Wallpaper"}/>
+            {width > 440 ? (<>
+                <img className={Styles.ImageAppearance} src={WallpaperAsset} alt={"Failed to load image: Wallpaper"}/>
+            </>) : (<>
+
+            </>) }
             <Overlay />
         </div>
     );
