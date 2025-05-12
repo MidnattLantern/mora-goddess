c63a78bd6e1ed4f6dbdd70f8e33d0ad6df426ebc
Alma <isakvent@outlook.com>
2025-05-12 17:21:36 +0200

    minimum viable variants for bust portrait

--- mora-goddess/src/components/projectBustPortrait/ProjectBustPortrait.tsx
+++ mora-goddess/src/components/projectBustPortrait/ProjectBustPortrait.tsx
@@ -2,11 +2,91 @@ import Styles from "./ProjectBustPortrait.module.css";
 import DownloadIcon from "../../assets/svg/DownloadIcon.svg?react";
 //import TurnPageIcon from "../../assets/svg/TurnPageIcon.svg?react";
 //import { useRenderPageContext } from "../../contexts/useRenderPageContext";
-import VariationFullSketchNoLogo from "../../assets/downloadable/variations/bust-portrait-full-sketch-nologo.png";
-import VariationFullSketchNoLogoSmall from "../../assets/downloadable/variations/bust-portrait-full-sketch-nologo-1mb.jpg";
+import BustNoLogo from "../../assets/downloadable/bust-portrait-nologo-8mb.png";
+import BustNoLogoSmall from "../../assets/downloadable/bust-portrait-nologo-160kb.jpg";
+import BustVariationFullNoLogo from "../../assets/downloadable/variations/bust-portrait-full-nologo-21mb.png";
+import BustVariationFullNoLogoSmall from "../../assets/downloadable/variations/bust-portrait-full-nologo-300kb.jpg";
+import BustVariationFullSketchNoLogo from "../../assets/downloadable/variations/bust-portrait-full-sketch-nologo-11mb.png";
+import BustVariationFullSketchNoLogoSmall from "../../assets/downloadable/variations/bust-portrait-full-sketch-nologo-1mb.jpg"
+import { useState } from "react";
+
 
 const ProjectBustPortrait = () => {
 //    const { setPageName } = useRenderPageContext();
+    const [showVariants, setShowVariants] = useState<boolean>(false);
+
+    const FeaturedImageView = () => {
+        return (
+            <>
+                <div className={Styles.ImageContainer}>
+
+                    <div className={Styles.ImageAspect}/>
+                        <img className={Styles.ImageSize} src={BustNoLogo} alt={"Failed to load image: Bust No Logo"}/>
+                    </div>
+                    <div className={Styles.NavigationBarContainer}>
+                        <a className={Styles.DownloadButton} href={BustNoLogo} download={"Bust No Logo"}>
+                            <DownloadIcon className={Styles.DownloadIcon}/>
+                            {"As png 8mb"}
+                        </a>
+
+                        <a className={Styles.DownloadButton} href={BustNoLogoSmall} download={"Bust No Logo"}>
+                            <DownloadIcon className={Styles.DownloadIcon}/>
+                            {"As jpg 160kb"}
+                        </a>
+
+                        {/*
+                        <button className={Styles.LinkToWeblogButton} onClick={() => {setPageName("weblog-bust-portrait")}}>
+                            {"Read the Weblog"}
+                            <TurnPageIcon className={Styles.TurnPageIcon}/>
+                        </button>
+                        */}
+
+                        <button className={Styles.VariantsButton} onClick={() => {setShowVariants(true)}}>
+                            {"Variants"}
+                        </button>
+                </div>
+            </>
+        )
+    };
+
+    const VariantsView = () => {
+        return(
+            <>
+                <h1>{"Bust"}</h1>
+                <a className={Styles.DownloadButton} href={BustNoLogo} download={"Bust No Logo"}>
+                    <DownloadIcon className={Styles.DownloadIcon}/>
+                    {"As png 8mb"}
+                </a>
+                <a className={Styles.DownloadButton} href={BustNoLogoSmall} download={"Bust No Logo"}>
+                    <DownloadIcon className={Styles.DownloadIcon}/>
+                    {"As jpg 160kb"}
+                </a>
+                <br/>
+
+                <h1>{"Full Body"}</h1>
+                <a className={Styles.DownloadButton} href={BustVariationFullNoLogo} download={"Full No Logo"}>
+                    <DownloadIcon className={Styles.DownloadIcon}/>
+                    {"As png 21mb"}
+                </a>
+                <a className={Styles.DownloadButton} href={BustVariationFullNoLogoSmall} download={"Full No Logo"}>
+                    <DownloadIcon className={Styles.DownloadIcon}/>
+                    {"As jpg 300kb"}
+                </a>
+                <br/>
+
+                <h1>{"Full Body Sketch"}</h1>
+                <a className={Styles.DownloadButton} href={BustVariationFullSketchNoLogo} download={"Full Sketch No Logo"}>
+                    <DownloadIcon className={Styles.DownloadIcon}/>
+                    {"As png 11mb"}
+                </a>
+                <a className={Styles.DownloadButton} href={BustVariationFullSketchNoLogoSmall} download={"Full Sketch No Logo"}>
+                    <DownloadIcon className={Styles.DownloadIcon}/>
+                    {"As jpg 1mb"}
+                </a>
+                <br/>
+            </>
+        )
+    };
 
     return (
         <>
@@ -16,30 +96,13 @@ const ProjectBustPortrait = () => {
                 </div>
 
                 <div className={Styles.ImageAndNavigationContainer}>
-                    <div className={Styles.ImageContainer}>
-                        <img className={Styles.ImageSize} src={VariationFullSketchNoLogo} alt={"Failed to load image: Variation Full Body Sketch"}/>
-                    </div>
-                    <div className={Styles.NavigationBarContainer}>
-                        <a className={Styles.DownloadButton} href={VariationFullSketchNoLogo} download={"variation full body sketch"}>
-                            <DownloadIcon className={Styles.DownloadIcon}/>
-                            {"As png 16mb"}
-                        </a>
+                    {showVariants ? <>
+                        <VariantsView />
+                    </> : <>
+                        <FeaturedImageView />
+                    </>}
 
-                        <a className={Styles.DownloadButton} href={VariationFullSketchNoLogoSmall} download={"variation full body sketch 1mb"}>
-                            <DownloadIcon className={Styles.DownloadIcon}/>
-                            {"As jpg 1mb"}
-                        </a>
-
-{/*
-                        <button className={Styles.LinkToWeblogButton} onClick={() => {setPageName("weblog-bust-portrait")}}>
-                            {"Read the Weblog"}
-                            <TurnPageIcon className={Styles.TurnPageIcon}/>
-                        </button>
-*/}
-
-                    </div>
                 </div>
-
             </div>
         </>
     );
